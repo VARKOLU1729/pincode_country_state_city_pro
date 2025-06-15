29e2a6b3274f9a449a2ec4a84ed404df24a2468d
varkolu1729 <varkolu.vijay1729@gmail.com>
2025-06-16 00:54:51 +0530

    added example template and all apis

---
+++ lib/src/models/city.dart
@@ -0,0 +1,40 @@
+class City {
+  final String name;
+  final String countryCode;
+  final String stateCode;
+  final String? latitude;
+  final String? longitude;
+  //the postal code for cities is generated using geocoding based on lat and long of cities- 04-06-2025
+  final String postalCode;
+  late final String isoCode;
+
+  City({
+    required this.name,
+    required this.countryCode,
+    required this.stateCode,
+    required this.postalCode,
+    this.latitude,
+    this.longitude,
+  }) {
+    isoCode = "${name}_$stateCode";
+  }
+
+  static City fromJson(Map<String, dynamic> json) => City(
+        name: json['name'],
+        countryCode: json['countryCode'],
+        stateCode: json['stateCode'],
+        latitude: json['latitude'],
+        longitude: json['longitude'],
+        postalCode: json['postalCode'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'countryCode': countryCode,
+        'stateCode': stateCode,
+        'latitude': latitude,
+        'longitude': longitude,
+        'isoCode': isoCode,
+        'postalCode': postalCode,
+      };
+}
