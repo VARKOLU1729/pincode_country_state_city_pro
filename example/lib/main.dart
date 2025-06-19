2952b25207465f51c2c2e923544aabf70fc9f2c3
varkolu1729 <varkolu.vijay1729@gmail.com>
2025-06-19 23:58:49 +0530

    added example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,8 +5,25 @@ void main() {
   AddressPickerController controller = AddressPickerController();
   runApp(MaterialApp(
     home: Scaffold(
-      body: PincodeCountryStateCityPicker(
-        controller: controller,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          "Pincode->Country->State->City",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "assets/yeah.png",
+              fit: BoxFit.cover, // or BoxFit.fill if you want exact stretch
+            ),
+          ),
+          PincodeCountryStateCityPicker(
+            controller: controller,
+          ),
+        ],
       ),
     ),
   ));
